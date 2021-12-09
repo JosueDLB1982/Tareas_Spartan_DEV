0cad805baeb4315f82b0034001fa967ee3be2e69
Josue_Lopez <josuedlopez1982@gmail.com>
2021-12-09 17:56:58 -0300

    Tarea de consumo de API refinada y terminada

--- Josue/tareaAPIJosue/tarea.js
+++ Josue/tareaAPIJosue/tarea.js
@@ -1,16 +1,32 @@
 const button = document.getElementById('button')
-const results = []
+const charactersList = []
 
 button.addEventListener('click', () => {
     axios({
         method: 'GET',
         url: 'https://api.got.show/api/general/characters'
     })
-    .then((res) => {
-        for (const characters of res.data.book) {
-            results.push(`Titulo: ${characters.titles}, Nombre: ${characters.name}, Casa: ${characters.house}\n`)
-        } results.splice(10)
-          console.log(...results)
-    })
-    .catch(err => console.log(err))
+        .then((res) => {
+            for (const characters of res.data.book) {
+                if (characters.titles.length !== 0) {
+                    charactersList.push(`${characters.titles}, ${characters.name}, ${characters.house}\n`)
+                } else charactersList.push(`No posee título, ${characters.name}, ${characters.house}\n`)
+
+                const varyResult = (char) => {
+                    const varyChar = char.sort(() => { return Math.random() - 0.5 })
+                    return varyChar
+                }
+                varyResult(charactersList)
+                charactersList.splice(10)
+            }
+            charactersList.map((res) => {
+                const list = document.getElementById('list')
+                const fragment = document.createDocumentFragment()
+                const listItem = document.createElement('li')
+                listItem.textContent = res
+                fragment.appendChild(listItem)
+                list.appendChild(fragment)
+            })
+            console.log(...charactersList)
+        }).catch(error => console.log(`${error} No hubo respuesta satisfactoria del servidor`))
 })
